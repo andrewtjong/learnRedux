85f3e05d4eb4cf41b4be03d9aa3d56af3314631f
andrewtjong <andrewttj@yahoo.com>
2020-11-16 19:14:08 +0100

    bring together all action creators and dispatched them

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import store from "./redux/index"
 import {changeCount} from "./redux/count"
+import {addFavoriteThing, removeFavoriteThing} from "./redux/favoriteThings"
+import {setYouTubeTitle, incrementViewCount, upvoteVideo, downvoteVideo} from "./redux/youTubeVideo"
+
 
 store.dispatch(changeCount(42))
+store.dispatch(addFavoriteThing("ANUEL AA"))
+store.dispatch(addFavoriteThing("Karol G)"))
+store.dispatch(removeFavoriteThing("Karol G"))
+store.dispatch(setYouTubeTitle("Diosa Remix"))
+store.dispatch(incrementViewCount())
+store.dispatch(upvoteVideo())
+store.dispatch(incrementViewCount())
+store.dispatch(upvoteVideo())
+store.dispatch(downvoteVideo())
